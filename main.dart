b05d807523a72ac8634cd497b747ca917a962ef5
AlexSklk <159269929+AlexSklk@users.noreply.github.com>
2024-02-12 16:59:11 +0300

    Add files via upload
    shelf

--- main.dart
+++ main.dart
@@ -1,83 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(MaterialApp(
-      home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-              Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.deepOrange),
-                  height: 100,
-                  width: 150,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Example 1',
-                    textDirection: TextDirection.ltr,
-                  ),
-                  ),
+  runApp(Container(
+    child: Column(
+      textDirection: TextDirection.ltr,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      verticalDirection: VerticalDirection.down,
+      children: <Widget>[
 
-                ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.amber),
-                height: 100,
-                width: 150,
-                alignment: Alignment.center,
-                child: const Text(
-                  'Example 2',
-                  textDirection: TextDirection.ltr,
-              ),
-                ),
-              ),
-            Align(
-              alignment: const Alignment(0.0, 0.5),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.redAccent),
-                height: 100,
-                width: 150,
-                alignment: Alignment.center,
-                child: const Text(
-                  'Example 3',
-                  textDirection: TextDirection.ltr,
-                ),
-              ),
-            ),
-            Align(
-              alignment: const Alignment(0.0, -0.5),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.limeAccent),
-                height: 100,
-                width: 150,
-                alignment: Alignment.center,
-                child: const Text(
-                  'Example 4',
-                  textDirection: TextDirection.ltr,
-                ),
-                ),
-              ),
+          Expanded(
+            child: Container(color: Colors.yellow,
 
 
 
-          ]
 
-          ),
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text('Flutter = FUN'),
+      ),
+            flex: 4,
         ),
+        Expanded(child: Container(color: Colors.pink),
+          flex: 1,
         ),
+        Expanded(child: Container(color: Colors.indigoAccent,),
+          flex: 1,
+        ),
+        Expanded(child: Container(color: Colors.green,),
+          flex: 4,
+        ),
+        Expanded(child: Container(color: Colors.black,),
+          flex: 4,
+        ),
+        Expanded(child: Container(color: Colors.deepOrange,),
+          flex: 1,
+        ),
+        Expanded(child: Container(color: Colors.limeAccent,),
+          flex: 1,
+        ),
+        Expanded(child: Container(color: Colors.red,),
+          flex: 4,
+        ),
+      ],
+    ),
 
-        )
-    );
 
+
+  ));
 }
